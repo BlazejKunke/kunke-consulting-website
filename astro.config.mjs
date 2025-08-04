f71b9d7cc3c1f74c8aa467824a6fb869dd92b5c8
Blazej Kunke <blazej.kunke@gmail.com>
2025-08-04 15:33:27 +0200

    security v2 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  security: {
+    checkOrigin: true
+  },
+  vite: {
+    define: {
+      __CSP_NONCE__: JSON.stringify(Math.random().toString(36).substring(2, 15))
+    }
+  }
+});
